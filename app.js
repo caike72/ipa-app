467acc399e44fd13ffa997fd47859c77a9ee9dde
Caike Laurentino <caike_tb@ymail.com>
2021-02-05 11:10:19 -0300

    Script único e manner of articulation

--- app.js
+++ app.js
@@ -13,31 +13,34 @@ class ConsonantClassification {
 }
 
 let consonants = Array()
-consonants.push(new ConsonantClassification('/m/', 'labial', 'nasal', null))
-consonants.push(new ConsonantClassification('/p/', 'labial', 'plosive', false))
-consonants.push(new ConsonantClassification('/b/', 'labial', 'plosive', true))
-consonants.push(new ConsonantClassification('/f/', 'labio-dental', 'fricative', false))
-consonants.push(new ConsonantClassification('/v/', 'labio-dental', 'fricative', true))
-consonants.push(new ConsonantClassification('/θ/', 'dental', 'fricative', false))
-consonants.push(new ConsonantClassification('/ð/', 'dental', 'fricative', true))
-consonants.push(new ConsonantClassification('/n/', 'alveolar', 'nasal', null))
-consonants.push(new ConsonantClassification('/t/', 'alveolar', 'plosive', false))
-consonants.push(new ConsonantClassification('/d/', 'alveolar', 'plosive', true))
-consonants.push(new ConsonantClassification('/s/', 'alveolar', 'fricative', false))
-consonants.push(new ConsonantClassification('/z/', 'alveolar', 'fricative', true))
-consonants.push(new ConsonantClassification('/l/', 'alveolar', 'approximant', null))
-consonants.push(new ConsonantClassification('/tʃ/', 'post-alveolar', 'plosive', false))
-consonants.push(new ConsonantClassification('/dʒ/', 'post-alveolar', 'plosive', true))
-consonants.push(new ConsonantClassification('/ʃ/', 'post-alveolar', 'fricative', false))
-consonants.push(new ConsonantClassification('/ʒ/', 'post-alveolar', 'fricative', true))
-consonants.push(new ConsonantClassification('/r/', 'post-alveolar', 'approximant', null))
-consonants.push(new ConsonantClassification('/j/', 'palatal', 'approximant', null))
-consonants.push(new ConsonantClassification('/ŋ/', 'velar', 'nasal', null))
-consonants.push(new ConsonantClassification('/k/', 'velar', 'plosive', false))
-consonants.push(new ConsonantClassification('/g/', 'velar', 'plosive', true))
-consonants.push(new ConsonantClassification('/x/', 'velar', 'fricative', false))
-consonants.push(new ConsonantClassification('/w/', 'velar', 'approximant', null))
-consonants.push(new ConsonantClassification('/h/', 'glottal', 'fricative', null))
+consonants.push(new ConsonantClassification('m', 'labial', 'nasal', null))
+consonants.push(new ConsonantClassification('p', 'labial', 'plosive', false))
+consonants.push(new ConsonantClassification('b', 'labial', 'plosive', true))
+consonants.push(new ConsonantClassification('f', 'labio-dental', 'fricative', false))
+consonants.push(new ConsonantClassification('v', 'labio-dental', 'fricative', true))
+consonants.push(new ConsonantClassification('θ', 'dental', 'fricative', false))
+consonants.push(new ConsonantClassification('ð', 'dental', 'fricative', true))
+consonants.push(new ConsonantClassification('n', 'alveolar', 'nasal', null))
+consonants.push(new ConsonantClassification('t', 'alveolar', 'plosive', false))
+consonants.push(new ConsonantClassification('d', 'alveolar', 'plosive', true))
+consonants.push(new ConsonantClassification('s', 'alveolar', 'fricative', false))
+consonants.push(new ConsonantClassification('z', 'alveolar', 'fricative', true))
+consonants.push(new ConsonantClassification('l', 'alveolar', 'approximant', null))
+consonants.push(new ConsonantClassification('tʃ', 'post-alveolar', 'plosive', false))
+consonants.push(new ConsonantClassification('dʒ', 'post-alveolar', 'plosive', true))
+consonants.push(new ConsonantClassification('ʃ', 'post-alveolar', 'fricative', false))
+consonants.push(new ConsonantClassification('ʒ', 'post-alveolar', 'fricative', true))
+consonants.push(new ConsonantClassification('r', 'post-alveolar', 'approximant', null))
+consonants.push(new ConsonantClassification('j', 'palatal', 'approximant', null))
+consonants.push(new ConsonantClassification('ŋ', 'velar', 'nasal', null))
+consonants.push(new ConsonantClassification('k', 'velar', 'plosive', false))
+consonants.push(new ConsonantClassification('g', 'velar', 'plosive', true))
+consonants.push(new ConsonantClassification('x', 'velar', 'fricative', false))
+consonants.push(new ConsonantClassification('w', 'velar', 'approximant', null))
+consonants.push(new ConsonantClassification('h', 'glottal', 'fricative', null))
+
+var mode = window.location.search
+mode = mode.replace('?', '')
 
 // limpa o campo de dialogo
 function clearAnswer() {
@@ -49,30 +52,55 @@ var randomConsonant = ''
 
 var generateConsonant = function () {
   randomConsonant = (consonants[(Math.floor(Math.random() * 25))])
+  console.log(randomConsonant)
 
   // coloca a consoante no span
-  document.getElementById('symbol').innerHTML = `What's the place of articulation of ${randomConsonant.symbol}?`
+  document.getElementById('symbol').innerHTML = `What's the place of articulation of [${randomConsonant.symbol}]?`
   clearAnswer()
-  document.getElementById('place').value = ''
+  document.getElementById(mode).value = ''
 }
+var placeValue = document.getElementById(mode)
 
 //pega opção escolhida pelo usuario 
 
-function verifyOption() {
-  var placeValue = document.getElementById('place')
+if (mode === 'place') {
+  function verifyOption() {
+    //PLACE OF ARTICULATION
+    if (placeValue.value === randomConsonant.place) {
+      document.getElementById('answer').style.color = '#01A787'
+      document.getElementById('answer').innerHTML = `You're right! The consonant [${randomConsonant.symbol}] is ${randomConsonant.place}.`
+    } else if (randomConsonant.place === undefined) {
+      document.getElementById('answer').innerHTML = `Please, select a consonant first.`
+    } else if (placeValue.value === '') {
+      document.getElementById('answer').innerHTML = `Please, select an answer first.`
+    } else {
+      document.getElementById('answer').style.color = '#FC6057'
+      document.getElementById('answer').innerHTML = `You're wrong! Try again or click the next button.`
+    }
+  }
+} else if (mode === 'manner') {
+  function verifyOption() {
+    //MANNER OF ARTIULATION
+    if (placeValue.value === randomConsonant.manner) {
+      document.getElementById('answer').style.color = '#01A787'
+      document.getElementById('answer').innerHTML = `You're right! The consonant [${randomConsonant.symbol}] is ${randomConsonant.manner}.`
+    } else if (randomConsonant.manner === undefined) {
+      document.getElementById('answer').innerHTML = `Please, select a consonant first.`
+    } else if (placeValue.value === '') {
+      document.getElementById('answer').innerHTML = `Please, select an answer first.`
+    } else {
+      document.getElementById('answer').style.color = '#FC6057'
+      document.getElementById('answer').innerHTML = `You're wrong! Try again or click the next button.`
+    }
+  }
+} else if (mode === 'voicing') {
+  function verifyOption() {
+    //VOICING
 
-  //retorno da resposta
+  }
+} else if (mode === 'all') {
+  function verifyOption() {
+    //ALL
 
-  if (placeValue.value === randomConsonant.place) {
-    document.getElementById('answer').style.color = '#01A787'
-    document.getElementById('answer').innerHTML = `You're right! The consonant ${randomConsonant.symbol} is ${randomConsonant.place}.`
-  } else if (placeValue.value === '') {
-    document.getElementById('answer').innerHTML = `Please, select an answer first.`
-  } else if (randomConsonant.place === undefined) {
-    document.getElementById('answer').innerHTML = `Please, select a consonant first.`
-  } else {
-    document.getElementById('answer').style.color = '#FC6057'
-    document.getElementById('answer').innerHTML = `You're wrong! Try again or click the next button.`
   }
 }
-
--- readme.md
+++ readme.md
@@ -1,21 +1,31 @@
-# MODO 1 - PLACE OF ARTICULATION
+# GERAL
 
-- [x] Criar a base HTML e estilizá-la
+- [x] Fazer com que possa selecionar um modo para que o script em página unica funcione.
 
-- [x] Criar uma lista com os fonemas e suas classificações
 
-- [x] Fazer com que um fonema aleatorio vá para o display
+# MODOS 1 E 2 - PLACE AND MANNER OF ARTICULATION
 
-- ~~[x] Gerar as opções de respostas~~
+- [x] Criar a base HTML e estilizá-la;
 
-- [x] Fazer com que a resposta certa retorne uma mensagem e manda outro fonema ou retorna uma mensagem de erro.
+- [x] Criar uma lista com os fonemas e suas classificações;
+
+- [x] Fazer com que um fonema aleatorio vá para o display;
+
+- [x] Fazer com que a resposta certa retorne uma mensagem e manda outro fonema ou retorna uma mensagem de erro;
 
 - [x] Repensar a forma como os fonemas são gerados, para que haja uma maneira de checar o resultado e também gerar um novo numero com o clicar de um botão.
 
-- ~~[x] Fazer com que as cores dos backgrounds resetem assim que o click no botão "Next" é feito.~~
-
-# MODO 2 - MANNER OF ARTICULATION
 
 # MODO 3 - VOICING
 
+- [] Pensar numa maneira de fazer este modo;
+
+- [] Planejar e codar.
+
+
+
 # MODO 4 - PLACE, MANNER AND VOICING
+
+- [] Pensar numa maneira de fazer este modo;
+
+- [] Planejar e codar.
